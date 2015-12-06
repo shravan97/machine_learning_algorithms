0a59f1cd564b72fa882efe510bd70bca6e25e21a
shravan97 <shravanmurali@gmail.com>
2015-12-06 22:16:44 +0530

    added random intialization

--- k-means.cpp
+++ k-means.cpp
@@ -11,19 +11,29 @@ typedef long long ll;
 main()
 {
 
-std::vector<pos> v;
+std::vector<point> v;
 
 long long int n,k;
 cout<<"Enter the number of points and the no. of clusters : ";
 cin>>n>>k;//Input total number of points and total number of clusters
 
+//Also assuming that number of clusters will be less than the number of points.
+
 cout<<"Enter the clusters : ";
 for(ll int i=0;i<n;i++)//Input all the co-ordinates in pairs
-{	point temp;
+{	
+	point temp;
 	cin>>temp.x>>temp.y;
 	cin>>v.push_back(temp);
 }
 
+std::vector<point> centroids;
+
+for (i = 0; i < k; ++i)
+{
+	centroids[i]=v[i];
+}
+//The above for loop randomly intializes cluster centroids to the first k input points.
 
 return 0;
 
