0135b07f361a94d5022e0f265334de1fad72405e
shravan97 <shravanmurali@gmail.com>
2015-12-06 17:56:14 +0530

    no. of inputs and clusters taken in

--- k-means.cpp
+++ k-means.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+struct pos
+{
+	int x ,y;
+};
+
+typedef long long ll;
 main()
 {
 
+std::vector<pos> v;
 
+long long int n,k;
+cout<<"Enter the number of points and the no. of clusters : ";
+cin>>n>>k;
+
+cout<<"Enter the clusters : ";
+//for(l)
 return 0;
 
 }
