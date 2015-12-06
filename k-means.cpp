04998569b1fe6faa6c1701c364945dc8e0cda0ae
shravan97 <shravanmurali@gmail.com>
2015-12-06 22:09:29 +0530

    added comments

--- k-means.cpp
+++ k-means.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 using namespace std;
 
-struct pos
+struct point
 {
 	int x ,y;
 };
-
+//The above structure represents each point
 typedef long long ll;
 main()
 {
@@ -15,14 +15,15 @@ std::vector<pos> v;
 
 long long int n,k;
 cout<<"Enter the number of points and the no. of clusters : ";
-cin>>n>>k;
+cin>>n>>k;//Input total number of points and total number of clusters
 
 cout<<"Enter the clusters : ";
-for(ll int i=0;i<n;i++)
-{	pos temp;
+for(ll int i=0;i<n;i++)//Input all the co-ordinates in pairs
+{	point temp;
 	cin>>temp.x>>temp.y;
 	cin>>v.push_back(temp);
 }
+
 
 return 0;
 
